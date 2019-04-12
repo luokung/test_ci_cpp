50d2f7c828df6f04d0ce451f8ed2664b921f57b2
范振华 <49389740+fanzhenhua0401@users.noreply.github.com>
2019-04-12 09:17:22 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int mult(int a, int b) {
+    return a * b;
+}
+
+int main() {
+    add(1,2);
+    mult(1,2);
+    cout << "hellloworld\n";
+    return 0;
+}
